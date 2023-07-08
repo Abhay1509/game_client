2c0fdb52bd2097e740eb2609458d4a06bd9c1230
Abhay1509 <abhayjaiswal333333.aj@gmail.com>
2023-07-09 04:35:30 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,52 +4,53 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [dataVal, setDataVal] = useState("");
+  const [data, setData] = useState([]);
 
-  const showDataHandler = () => {
-    axios
-      .get("http://192.168.29.74:5173/getAllCollections")
-      .then(function (response) {
-        const dataId = JSON.stringify(response.data);
-        setDataVal(dataId);
-        console.log("resp :: ", response.data);
-      })
-      .catch(function (error) {
-        console.log("error :: ", error);
-      });
-  };
-  
-
-  const addHandler = async () => {
+  const fetchData = async () => {
     try {
-      const response = await axios.post("http://192.168.29.74:5173/addNewRow", {
-        name: "Abhay",
-        num: 10,
-      });
-
-      console.log(response.data);
+      const response = await axios.get(
+        "http://172.16.2.119:5173/getAllCollections"
+      );
+      setData(response.data);
     } catch (error) {
       console.error(error);
     }
   };
 
-  //     function Create() {
-  //     const [name, setName] = useState('');
-  //     const [num, setNum] = useState('');
-  //     const postData = () => {
-  //       axios.post(`http://192.168.29.74:5173/addNewRow`, {
-  //           name,
-  //           num
-  //       })
-  //   }
-  // }
+  const showDataHandler = () => {
+    fetchData();
+  };
+
+  const postData = async () => {
+    try {
+      const response = await axios.post("http://172.16.2.119:5173/addNewRow", {
+        name: "Sanchit",
+        age: 27,
+      });
+      console.log(response.data);
+      console.log(data);
+      setData([...data, response.data]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
-      <button onClick={showDataHandler}>Show Data base</button>
-      <p>{dataVal}</p>
+      <div>
+        <h2>Previous Data</h2>
+        <ul>
+          {data.map((item, index) => (
+            <li key={index}>
+              Name:{item.name} & Age:{item.age}
+            </li>
+          ))}
+        </ul>
+      </div>
 
-      <button onClick={addHandler}>Add Data</button>
+      <button onClick={showDataHandler}>Show Data base</button>
+
+      <button onClick={postData}>Add Data</button>
     </>
   );
 }
