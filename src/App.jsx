7652e00652cadac26650188f51fc6c591c64c3d0
Abhay1509 <abhayjaiswal333333.aj@gmail.com>
2023-07-26 23:41:39 +0530

    ip fetchong done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "./App.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-function App() {
+function App({ serverIP, serverPort }) {
   const [data, setData] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
 
   const fetchData = async () => {
     try {
       const response = await axios.get(
-        "http://192.168.69.13:5173/getAllCollections"
+        `http://${serverIP}:${serverPort}/getAllCollections`
       );
       setData(response.data);
     } catch (error) {
@@ -21,23 +23,42 @@ function App() {
     fetchData();
   };
 
-  
- 
   const postData = async () => {
     try {
-      const response = await axios.post("http://192.168.69.13:5173/addNewRow", {
-        name: "rakesh",
-        age: 110,
-      });
+      const response = await axios.post(
+        "http://${serverIP}:${serverPort}/addNewRow",
+        {
+          name: name,
+          age: age,
+        }
+      );
       // fetchData();
       console.log(response.data);
       // setData([...data, response.data]);
-      setData(response.data.message)
+      setData(response.data.message);
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handleAgeChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const handleDelete = async (itemId) => {
+    try {
+      await axios.delete(
+        `http://${serverIP}:${serverPort}/deleteRow/${itemId}`
+      );
+      const updatedData = data.filter((item) => item.id !== itemId);
+      setData(updatedData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   // useEffect(() => {
   //   fetchData();
   // }, [postData]);
@@ -48,7 +69,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="bg-red">
         <h2>Data</h2>
         <ul>
           {data.map((item, index) => (
@@ -58,10 +79,42 @@ function App() {
           ))}
         </ul>
       </div>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlInput1" className="form-label">
+          Name
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="exampleFormControlInput1"
+          value={name}
+          onChange={handleNameChange}
+          placeholder="Enter your Name"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlInput1" className="form-label">
+          Age
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="exampleFormControlInput1"
+          value={age}
+          onChange={handleAgeChange}
+          placeholder="Enter your Name"
+        />
+        <button className="btn btn-primary" onClick={postData}>
+          Add Data
+        </button>
+      </div>
 
-      <button onClick={showDataHandler}>Show Data base</button>
-
-      <button onClick={postData}>Add Data</button>
+      <button className="btn btn-success" onClick={showDataHandler}>
+        Show Data base
+      </button>
+      <button className="btn btn-danger" onClick={handleDelete}>
+        Delete
+      </button>
     </>
   );
 }
